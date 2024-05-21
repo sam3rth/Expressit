305564da51feb9f647154e669d47ef244f55b1e1
sam3rth <samarthtyagi001@gmail.com>
2024-05-21 21:25:43 +0530

    frontend completed

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector('.login-button').addEventListener('click', function() {
+    window.location.href = 'login.html';
+});
+
+document.querySelector('.signup-button').addEventListener('click', function() {
+    window.location.href = 'signup.html';
+});
